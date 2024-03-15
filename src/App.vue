f19b949685888f1576154e08128faf58b6facd94
fabienstrauss <spaklhd@gmail.com>
2024-03-15 14:21:50 +0100

    first change commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue' 
 import TheWelcome from './components/TheWelcome.vue'
+import Header from './components/Header.vue'
 </script>
 
 <template>
   <header>
+    <Header />
+    <!--
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div> 
+  -->
   </header> 
 
   <main>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="header"> </div>
+</template>
+
+<style scoped>
+    .header {
+        width: 100%;
+        height: 100px;
+        min-height: 100px;
+        color:black;
+        position: absolute;
+    }
+</style>
