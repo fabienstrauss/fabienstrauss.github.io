4b5a6454e4b37b0df15eeeeac16b27576dfbb907
fabienstrauss <spaklhd@gmail.com>
2024-03-15 15:06:41 +0100

    fix routing

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,6 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'; // Adjust the path according to your file structure
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app')
