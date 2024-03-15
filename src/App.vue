0362034e9c56767aa0ea8a51fbb4c76094bbac3d
fabienstrauss <spaklhd@gmail.com>
2024-03-15 14:50:44 +0100

    test

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
     <router-link to="/profile">Profile</router-link>
   </div>
   <router-view/>
