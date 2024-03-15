4f663a212b0baa253bc846ce6ea65df3185f66bd
fabienstrauss <spaklhd@gmail.com>
2024-03-15 14:42:13 +0100

    added routing

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../view/Home.vue'
+import Profile from '../views/Profile.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+export default router
