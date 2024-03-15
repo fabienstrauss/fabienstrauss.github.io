ac60bab164e738748fba17c02a51b38834c3aac8
fabienstrauss <spaklhd@gmail.com>
2024-03-15 16:19:26 +0100

    fix routing views

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Profile from '../views/Profile.vue'
+import About from '../views/About.vue'
 const routes = [
   {
     path: '/',
@@ -14,11 +15,8 @@ const routes = [
   },
   {
     path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    name: 'about',
+    component: About,
   }
 ]
 const router = createRouter({
