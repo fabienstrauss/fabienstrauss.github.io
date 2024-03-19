6f7c30ad948916a685c8c85b1a887311bf8acd6f
fabienstrauss <spaklhd@gmail.com>
2024-03-19 20:26:30 +0100

    app.vue changes

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,42 @@
 </script>
 
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/work">Work</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+
+    <!-- -- Navbar -- -->
+    <nav id="navbar">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/work">Work</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+
+    <!-- -- Main Content Area -- -->
+    <main id="content">
+      <router-view/>
+    </main>
   </div>
-  <router-view/>
 </template>
 
 <style scoped> 
+
+  #app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+  }
+
+  #navbar {
+    height: 100px;
+    width: 100%;
+    background-color: #333;
+    color: white;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+  }
+
+  #content {
+    flex-grow: 1;
+  }
 
 </style>
